a83e978476d92feb90f7f82de113997fd5733c7d
leandromaydana <leandro-maydana@hotmail.com>
2023-08-10 00:25:05 -0300

    tarea clase 06

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+const errorHandler = require("./middleware/errorHandler");
+const autenticacion = auth({
+    audience: "http://localhost:3000/api/productos",
+    issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
+    tokenSigningAlg: "RS256",
+});
 const app = express();
 app.use(express.json());
-const librosRouter = require('./routes/libros');
-
-const errorHandler = require('./middleware/errorHandler');
-app.use('/libros', librosRouter);
+const librosRouter = require("./routes/libros");
+app.use("/libros", autenticacion, librosRouter);
 app.use(errorHandler);
 app.listen(3000, () => {
-    console.log('Servidor iniciado en el puerto 3000');
+    console.log("Servidor iniciado en el puerto 3000");
 });
--- routes/libros.js
+++ routes/libros.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Libro = require("../models/Libro");
-router.get("/", async (req, res) => {
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
+router.get("/", requiredScopes("read:libros"), async (req, res) => {
     try {
         const libros = await Libro.find();
         res.json(libros);
@@ -9,7 +10,7 @@ router.get("/", async (req, res) => {
         res.status(500).json({ error: "Error al obtener los libros" });
     }
 });
-router.post("/", async (req, res) => {
+router.post("/", requiredScopes("write:libros"), async (req, res) => {
     try {
         const nuevoLibro = new Libro(req.body);
         await nuevoLibro.save();
@@ -18,7 +19,7 @@ router.post("/", async (req, res) => {
         res.status(500).json({ error: "Error al crear el Libro" });
     }
 });
-router.put("/:id", async (req, res) => {
+router.put("/:id", requiredScopes("write:libros"), async (req, res) => {
     try {
         const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body,
             {
@@ -29,12 +30,12 @@ router.put("/:id", async (req, res) => {
         res.status(500).json({ error: "Error al actualizar el Libro" });
     }
 });
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", requiredScopes("write:libros"), async (req, res) => {
     try {
         await Libro.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Libro eliminado correctamente' });
+        res.json({ message: "Libro eliminado correctamente" });
     } catch (error) {
-        res.status(500).json({ error: 'Error al eliminar el Libro' });
+        res.status(500).json({ error: "Error al eliminar el Libro" });
     }
 });
 module.exports = router;
